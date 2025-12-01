3079b312019e381b8f7a32dac2e16ab5d4811551
Millian F <96703882+NightmareChamillian@users.noreply.github.com>
2025-12-01 18:01:18 -0500

    Hashes the absolute basic web structure out

--- sketch.js
+++ sketch.js
@@ -1,5 +1,15 @@
 function setup() {
-  createCanvas(400, 400);
+  var ourCanv = createCanvas(600, 1400);
+  let newCanvasY = (windowHeight)/2;
+  ourCanv.position = (ourCanv.position.x, newCanvasY + 1700);
+}
+
+var testy;
+
+function doWeave(){
+  testy = document.getElementById("textInput").value;
+  document.getElementById("feedback").innerHTML = testy;
+  draw();
 }
 
 function draw() {
